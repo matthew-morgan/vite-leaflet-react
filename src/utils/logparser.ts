bfaa147643891c341d66451bce5d4ef55a99019c
Matthew Morgan <matthew@hidlen.com>
2023-04-13 11:48:31 +0100

    Add GPS timestamp

--- src/utils/logparser.ts
+++ src/utils/logparser.ts
@@ -1,4 +1,7 @@
 interface LogLine {
+    hours: number;
+    minutes: number;
+    seconds: number;
     timestamp: number;
     latitude: number;
     longitude: number;
@@ -15,18 +18,23 @@ interface LogLine {
 function parseLogLine(logLineStr: string): LogLine {
     const parts = logLineStr.split(',');
 
+    const timeParts = parts[0].split(':');
+
     const logLine: LogLine = {
-        timestamp: parseFloat(parts[0]),
-        latitude: parseFloat(parts[1]),
-        longitude: parseFloat(parts[2]),
-        altitude: parseFloat(parts[3]),
-        encoderDistance: parseInt(parts[4], 10),
-        accelX: parseFloat(parts[5]),
-        accelY: parseFloat(parts[6]),
-        accelZ: parseFloat(parts[7]),
-        gyroX: parseFloat(parts[8]),
-        gyroY: parseFloat(parts[9]),
-        gyroZ: parseFloat(parts[10]),
+        hours: parseInt(timeParts[0], 10),
+        minutes: parseInt(timeParts[1], 10),
+        seconds: parseInt(timeParts[2], 10),
+        timestamp: parseFloat(parts[1]),
+        latitude: parseFloat(parts[2]),
+        longitude: parseFloat(parts[3]),
+        altitude: parseFloat(parts[4]),
+        encoderDistance: parseInt(parts[5], 10),
+        accelX: parseFloat(parts[6]),
+        accelY: parseFloat(parts[7]),
+        accelZ: parseFloat(parts[8]),
+        gyroX: parseFloat(parts[9]),
+        gyroY: parseFloat(parts[10]),
+        gyroZ: parseFloat(parts[11]),
     };
 
     return logLine;
@@ -35,6 +43,6 @@ function parseLogLine(logLineStr: string): LogLine {
 export { parseLogLine };
 
 // Example usage
-const exampleLogLine = "162345,37.421998,-122.084097,10.0,25,0.01,0.02,-0.03,1.2,2.3,-0.5";
+const exampleLogLine = "12:34:56,162345,37.421998,-122.084097,10.0,25,0.01,0.02,-0.03,1.2,2.3,-0.5";
 const parsedLogLine = parseLogLine(exampleLogLine);
 console.log(parsedLogLine);
